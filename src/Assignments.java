7a46c0c1a4e22def396df7282cec460c5c467467
Mason Parry <mparry27@bruinmail.slcc.edu>
2017-12-01 14:26:37 -0700

    The changes made to the java files. Also added the Assignemtns class

---
+++ src/Assignments.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Assignments {
+	//Fields
+	static private ArrayList<Assignments> assignments;
+	
+	//Methods
+	static public ArrayList<Assignments> getAssignments(){
+		return assignments;
+	}
+}
--- src/GradeBookGUI.java
+++ src/GradeBookGUI.java
@@ -2,8 +2,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
-import java.awt.BorderLayout;
-import java.awt.Component;
+
 import javax.swing.table.DefaultTableModel;
 import java.awt.Color;
 
@@ -11,8 +10,6 @@ public class GradeBookGUI {
 
 	private JFrame frame;
 	private JTable table;
-	private JTable table_1;
-	private JTable table_2;
 
 	/**
 	 * Launch the application.
@@ -46,38 +43,18 @@ public class GradeBookGUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		table_1 = new JTable();
-		table_1.setModel(new DefaultTableModel(
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
+				{"Hello", "World", "I'm", "Alive"},
 			},
 			new String[] {
-				"Student ID", "First", "Last", "Grade", "GPA"
+				"Student ID", "Name", "Grade", "GPA"
+				
 			}
 		));
-		table_1.setBackground(Color.WHITE);
-		table_1.setBounds(12, 26, 331, 81);
-		frame.getContentPane().add(table_1);
-		
-		table_2 = new JTable();
-		table_2.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-			},
-			new String[] {
-				"Assignement 1", "Quiz 1", "Test 1"
-			}
-		));
-		table_2.setBounds(344, 26, 426, 81);
-		frame.getContentPane().add(table_2);
-
+		table.setBackground(Color.WHITE);
+		table.setBounds(12, 13, 758, 94);
+		frame.getContentPane().add(table);
 	}
 }
