53572cf0dd508ea9a28af485f77794ed36e931e5
David Jones <emaildkj@gmail.com>
2017-12-04 14:02:37 -0700

    Created methods in the static Assignments class and made the Assignment class.

--- src/Assignments.java
+++ src/Assignments.java
@@ -1,11 +1,53 @@
 import java.util.ArrayList;
 
-public class Assignments {
+/**
+ * class that keeps track of all assignments.
+ * @author david
+ */
+public class Assignments 
+{
 	//Fields
-	static private ArrayList<Assignments> assignments;
+	static private ArrayList<Assignment> assignments;
 	
 	//Methods
-	static public ArrayList<Assignments> getAssignments(){
+	static public ArrayList<Assignment> getAssignments()
+	{
 		return assignments;
+	}
+	
+	/**
+	 * Gets a specific assignment by index.
+	 * @return
+	 */
+	static public Assignment getAssignment( int index )
+	{
+		return assignments.get(index);
+	}
+	
+	/**
+	 * Add an assignment to the assignment list.
+	 * @return
+	 */
+	static public void addAssignment( Assignment assignment )
+	{
+		assignments.add(assignment);
+	}
+	
+	/**
+	 * Remove an assignment from the assignment list.
+	 * @return
+	 */
+	static public void removeAssignment (Assignment assignment)
+	{
+		assignments.remove(assignments);
+	}
+	
+	/**
+	 * Remove assignment from the list by index.
+	 * @return
+	 */
+	static public void removeAssignmentAt( int index )
+	{
+		assignments.remove(index);
 	}
 }
